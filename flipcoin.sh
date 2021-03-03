a67e7fd9d325284bcd73eaee3cb08834487cb6bb
kartik <kavimandankartik33@gmail.com>
2021-03-02 23:17:57 -0800

    Added: flip coin 11 time check who win max

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,2 +1,29 @@
-#Welcome to flipcoin simulation problems
 
+#!/bin/bash
+
+echo "flip Coin Simulator"
+
+echo "number of times user want to flip the coin"
+read flip
+echo $flip
+countHead=0
+countTail=0
+for((i=0;i<flip;i++))
+do
+if [ $((RANDOM%2)) -eq 1 ]
+then
+((countHead++))
+else
+((countTail++))
+fi
+done
+echo "head="$countHead
+echo "tail="$countTail
+
+if
+ [ $countHead -gt $countTail ]
+then
+echo "countHead wins"
+else
+echo "countTail win"
+fi
