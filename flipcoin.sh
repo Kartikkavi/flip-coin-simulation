abdcdfaca91e9536b76845be6dda6e5c018747f6
kartik <kavimandankartik33@gmail.com>
2021-03-03 16:15:13 -0800

    Added: single coin flip

--- flipcoin.sh
+++ flipcoin.sh
@@ -6,8 +6,10 @@ echo "flip Coin Simulator"
 echo "number of times user want to flip the coin"
 read flip
 echo $flip
+
 countHead=0
 countTail=0
+declare -A noOfCounts
 for((i=0;i<flip;i++))
 do
 if [ $((RANDOM%2)) -eq 1 ]
@@ -20,10 +22,12 @@ done
 echo "head="$countHead
 echo "tail="$countTail
 
-if
- [ $countHead -gt $countTail ]
-then
-echo "countHead wins"
-else
-echo "countTail win"
-fi
+noOfCounts[tail]=$countTail
+noOfCounts[head]=$countHead
+
+echo ${noOfCounts[@]}
+
+PercentageOfTails=$((100*$countTail/$flip))
+echo "Percentage of tails= "$PercentageOfTails
+PercentageOfHeads=$((100*$countHead/$flip))
+echo "Percentage of heads= "$PercentageOfHeads
